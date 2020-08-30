263afa748e14ab3c9f4dba4458d5644bc786d0b8
Jserrano27 <jserrano.vd@gmail.com>
2020-08-30 18:36:41 -0300

    (red - green - refactor - repeat) and baby steps

--- parte2/src/OrderBundle/Service/CustomerCategoryService.php
+++ parte2/src/OrderBundle/Service/CustomerCategoryService.php
@@ -2,12 +2,23 @@
 
 namespace OrderBundle\Service;
 
+use OrderBundle\Entity\Customer;
+
 class CustomerCategoryService
 {
     const CATEGORY_NEW_USER = 'new-user';
+    const CATEGORY_LIGHT_USER = 'light-user';
+    const CATEGORY_MEDIUM_USER = 'medium-user';
+    const CATEGORY_HEAVY_USER = 'heavy-user';
 
-    public function getUsageCategory()
+    public function getUsageCategory(Customer $customer)
     {
+        if(
+            $customer->getTotalOrders() >= 5 &&
+            $customer->getTotalRatings() >= 1 &&
+            $customer->getTotalRecommendations() >= 0
+        ) { return self::CATEGORY_LIGHT_USER; }
+
         return self::CATEGORY_NEW_USER;
     }
 }
--- parte2/src/OrderBundle/Test/Service/CustomerCategoryServiceTest.php
+++ parte2/src/OrderBundle/Test/Service/CustomerCategoryServiceTest.php
@@ -29,4 +29,20 @@ class CustomerCategoryServiceTest extends TestCase
 
         $this->assertEquals(CustomerCategoryService::CATEGORY_NEW_USER, $usageCategory);
     }
+
+    /**
+     * @test
+     */
+    public function customerShouldBeLightUser()
+    {
+        $customerCategoryService = new CustomerCategoryService();
+
+        $customer = new Customer();
+        $customer->setTotalOrders(5);
+        $customer->setTotalRatings(1);
+
+        $usageCategory = $customerCategoryService->getUsageCategory($customer);
+
+        $this->assertEquals(CustomerCategoryService::CATEGORY_LIGHT_USER, $usageCategory);
+    }
 }
